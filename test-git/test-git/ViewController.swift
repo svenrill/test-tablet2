072e635852b8b289d53010c5bada17a11317c6e9
Sven Rill <sven.rill@hof-university.de>
2018-04-13 11:46:59 +0200

    initial version

---
+++ test-git/test-git/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test-git
+//
+//  Created by Sven on 13.04.18.
+//  Copyright © 2018 Sven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
